111ba7126d52af182b36f8a9bebe9306b39f4579
Vineet Sista <vineet.sista@gmail.com>
2025-11-07 23:21:09 -0500

    updated app.py

--- backend/learners/app.py
+++ backend/learners/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.users import users_bp
+from learners.routes.users import users_bp
 
-app = Flask(__name__)
-CORS(app, resources={r"/learners/*": {"origins": "*"}})
 
-app.register_blueprint(users_bp)
+def create_app():
+    app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    app.register_blueprint(users_bp, url_prefix="/learners")
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(port=5000, debug=True)
